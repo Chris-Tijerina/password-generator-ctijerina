dea935e83958fdacef4b072e6b5ea98f7ef0335c
Chris T <ctijerina@live.com>
2021-11-21 14:54:18 -0600

    created password string from criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
   //criteria variables
-  var lowerCase = window.prompt ("Do you want lowercase letters?")
-  var upperCase = window.prompt ("Do you want upppercase letters?")
-  var numeric = window.prompt ("Do you want numeric values?")
-  var special = window.prompt ("Do you want special characters?")
+  var lowerCase = window.prompt ("Do you want lowercase letters? yes or no")
+  var upperCase = window.prompt ("Do you want upppercase letters? yes or no")
+  var numeric = window.prompt ("Do you want numeric values? yes or no")
+  var special = window.prompt ("Do you want special characters? yes or no")
   var length = window.prompt ("How long should the password be? (8-128)")
   
   
-  var password = generatePassword();
+  var password = generatePassword(length, lowerCase, upperCase, numeric, special);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -25,8 +25,38 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(lowerCase, upperCase, numeric, special, length) {
-  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+function generatePassword(length, lowerCase, upperCase, numeric, special) {
+  var alphabetLower = "abcdefghijklmnopqrstuvwxyz"
+  var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var numbers = "0123456789"
   var characters = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~"
+  var password = ""
+
+
+
+  for (var i=0; i < length;) {
+    if (lowerCase === "yes" && i < length) {
+      password +=alphabetLower[Math.floor(Math.random()* alphabetLower.length)]
+      i++
+    }
+
+    if (upperCase === "yes" && i < length) {
+      password +=alphabetUpper[Math.floor(Math.random()* alphabetUpper.length)]
+      i++
+    }
+    if (numeric === "yes" && i < length) {
+      password +=numbers[Math.floor(Math.random()* numbers.length)]
+      i++
+    }
+    if (special === "yes" && i < length) {
+      password +=characters[Math.floor(Math.random()* characters.length)]
+      i++
+    }
+
+
+  }
+
+  console.log(password)
+
+
 }
