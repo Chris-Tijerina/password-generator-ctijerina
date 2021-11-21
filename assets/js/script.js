8e52af425d6657aa717d09421c3a03235c331606
Chris T <ctijerina@live.com>
2021-11-21 13:45:28 -0600

    criteria variables created and character banks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  //criteria variables
+  var lowerCase = window.prompt ("Do you want lowercase letters?")
+  var upperCase = window.prompt ("Do you want upppercase letters?")
+  var numeric = window.prompt ("Do you want numeric values?")
+  var special = window.prompt ("Do you want special characters?")
+  var length = window.prompt ("How long should the password be? (8-128)")
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +24,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(lowerCase, upperCase, numeric, special, length) {
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  var numbers = "0123456789"
+  var characters = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~"
+}
